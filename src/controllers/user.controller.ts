61d3769af028b7fb9fb9228ae052b8feb8a789ac
Patryk Karasiński <patryk.karasinski@treesat.io>
2024-10-13 18:52:04 +0200

    Create DELETE /users/:id route

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,8 @@
 import {Response, Request} from 'express';
-import pool from '../db';
-import {User} from '../types/user';
 import jwt from 'jsonwebtoken';
+import pool from '../db';
 import {getRole} from '../utils/role';
+import {User} from '../types/user';
 
 export const getLoggedUser = async (req: Request, res: Response) => {
   if (typeof process.env.JWT_KEY !== 'string') {
@@ -65,6 +65,35 @@ export const getUser = async (req: Request, res: Response) => {
     res.json(result[0]);
   } catch (error) {
     console.error('Error fetching user:', error);
+    res.status(500).json('Internal Server Error');
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const {id} = req.params;
+
+    if (!id) {
+      return res.status(400).json('Invalid ID.');
+    }
+
+    const sql1 = 'SELECT id, role_id FROM `users` WHERE id = ?';
+    const connection = await pool.getConnection();
+    const [result] = await connection.execute<User[]>(sql1, [id]);
+    connection.release();
+    if (result.length === 0) {
+      return res.status(403).json('User not found.');
+    }
+    const role = await getRole(result[0].role_id);
+    if (role === 'admin') {
+      return res.status(403).json('Not allowed to delete this user.');
+    }
+    const sql2 = 'DELETE FROM `users` WHERE id = ?';
+    await connection.execute(sql2, [id]);
+    connection.release();
+    res.status(200).json('User deleted successfully.');
+  } catch (error) {
+    console.error('User :: deleteUser', error);
     res.status(500).json('Internal Server Error');
   }
 };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getLoggedUser, getUser} from '../controllers/user.controller';
+import {getLoggedUser, getUser, deleteUser} from '../controllers/user.controller';
 import checkAuth from '../middleware/check-auth';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.use(checkAuth);
 router.get('/me', getLoggedUser);
 router.get('/:id', getUser);
+router.delete('/:id', deleteUser);
 
 export default router;
