a40602e554b9623ae7d0444bfc8f1064503926e9
Patryk Karasiński <patryk.karasinski@treesat.io>
2024-10-19 16:13:32 +0200

    Create DELETE /receipts/:id route

--- src/controllers/receipt.controller.ts
+++ src/controllers/receipt.controller.ts
@@ -18,3 +18,28 @@ export const getReceipts = async (req: Request, res: Response) => {
     res.status(500).json('Internal Server Error');
   }
 };
+
+export const deleteReceipt = async (req: Request, res: Response) => {
+  try {
+    const {id} = req.params;
+
+    if (!id) {
+      return res.status(400).json('Invalid ID.');
+    }
+
+    const sql1 = 'SELECT id FROM `receipts` WHERE id = ?';
+    const connection = await pool.getConnection();
+    const [result] = await connection.execute<Receipt[]>(sql1, [id]);
+    connection.release();
+    if (result.length === 0) {
+      return res.status(403).json('Receipt not found.');
+    }
+    const sql2 = 'DELETE FROM `receipts` WHERE id = ?';
+    await connection.execute(sql2, [id]);
+    connection.release();
+    res.status(200).json('Receipt deleted successfully.');
+  } catch (error) {
+    console.error('Receipt :: deleteReceipt', error);
+    res.status(500).json('Internal Server Error');
+  }
+};
--- src/routes/receipt.routes.ts
+++ src/routes/receipt.routes.ts
@@ -1,8 +1,13 @@
 import express from 'express';
-import {getReceipts} from '../controllers/receipt.controller';
+import {getReceipts, deleteReceipt} from '../controllers/receipt.controller';
+import checkAuth from '../middleware/check-auth';
 
 const router = express.Router();
 
 router.get('/', getReceipts);
+router.use((req, res, next) => {
+  checkAuth(req, res, next, true);
+});
+router.delete('/:id', deleteReceipt);
 
 export default router;
